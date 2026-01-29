6df89cec74917c777b672743c2eded00b545b127
Jeremy Winnett <winnettjeremy@gmail.com>
2026-01-28 21:52:01 -0500

    added win conditions function

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
-
-
-
 function Gameboard() {
   const rows = 3;
   const columns = 3;
-  const board = [];
+  let board = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
 
   for (let i = 0; i < rows; i++) {
     board[i] = [];
@@ -16,7 +16,6 @@ function Gameboard() {
 
   const getBoard = () => board;
   const placeMark = (column, row, player) => {
-
     if(board[row][column].getValue() === 0) {
       board[row][column].addMark(player)
     } else {
@@ -44,11 +43,14 @@ function Cell() {
   };
 }
 
+
+
 function GameController(
   playerOne = "Player One",
   playerTwo = "Player Two"
 ) {
   const board = Gameboard();
+  const cell = Cell()
   const players = [
     {
       name: playerOne,
@@ -78,17 +80,53 @@ function GameController(
       console.log("Choose again!");
     }
 
+  function winCondition () {
+    const b = board.getBoard().map(row => row.map(cell => cell.getValue()));
 
-    checkRow
+    for (let i = 0; i < 3; i++) {
+      if (b[i][0] !== 0 && b[i][0] === b[i][1] && b[i][1] === b[i][2]) {
+        console.log(`${b[i][0]} wins!`);
+        return true;
+      }
+    }
+
+      // Check columns
+    for (let i = 0; i < 3; i++) {
+      if (b[0][i] !== 0 && b[0][i] === b[1][i] && b[1][i] === b[2][i]) {
+        console.log(`${b[0][i]} wins!`);
+        return true;
+      }
+    }
+    // Check diagonals
+    if (b[0][0] !== 0 && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
+      console.log(`${b[0][0]} wins!`);
+      return true;
+    }
+    if (b[0][2] !== 0 && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
+      console.log(`${b[0][2]} wins!`);
+      return true;
+    }
+    return false;
+  }
 
     //add logic to check for winner here
+    winCondition();
     printNewRound();
   };
   printNewRound();
   return {
-    playRound
+    playRound, printNewRound
   };
+  
 }
 
 const game = GameController();
 window.playRound = game.playRound;
+
+const play = game.playRound;
+game.playRound(0,0);
+game.playRound(0,1);
+game.playRound(1,0);
+game.playRound(1,1);
+game.playRound(2,0);
+
