e4d5aa289fd31463fac28023bc4d961f9e645736
Jeremy Winnett <winnettjeremy@gmail.com>
2026-02-03 21:13:57 -0500

    added clickable game tiles added clickable reset button

--- script.js
+++ script.js
@@ -17,11 +17,12 @@ function Gameboard() {
       return
     }
   };
+
   const printBoard = () => {
   const boardValues = board.map((row) => row.map((cell) => cell.getValue()))
     console.log(boardValues);
   };
-    return { getBoard, placeMark, printBoard};
+    return { getBoard, placeMark, printBoard, placeMark};
 };
 
 
@@ -37,10 +38,19 @@ function Cell() {
   };
 };
 
+let gameOver = false;
+
+//function endGame() {
+  //if(gameOver === true) {
+    //console.log("Game Over");
+  //}
+//};
+
 function GameController(
   playerOne = "Player One",
   playerTwo = "Player Two"
 ) {
+
   const board = Gameboard();
   const players = [
     {
@@ -68,6 +78,7 @@ function GameController(
       if (b[i][0] !== 0 && b[i][0] === b[i][1] && b[i][1] === b[i][2]) {
         console.log(`${getActivePlayer().name} wins!`);
         board.printBoard()
+        gameOver = true;
         return true;
       }
     }
@@ -75,81 +86,86 @@ function GameController(
       if (b[0][i] !== 0 && b[0][i] === b[1][i] && b[1][i] === b[2][i]) {
         console.log(`${getActivePlayer().name} wins!`);
         board.printBoard()
+        gameOver = true;
         return true;
       }
     }
     if (b[0][0] !== 0 && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
       console.log(`${getActivePlayer().name} wins!`);
       board.printBoard()
+      gameOver = true;
       return true;
     }
     if (b[0][2] !== 0 && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
-      console.log(`${getActivePlayer().name}} wins!`);
+      console.log(`${getActivePlayer().name} wins!`);
       board.printBoard()
+      gameOver = true;
       return true;
     }
     return false;
   }
-  const playRound = (column, row) => {
-    printNewRound();
-    if (board.getBoard()[row][column].getValue() === 0) {
-      board.placeMark(column, row, getActivePlayer().mark);
-      console.log(`Placing ${getActivePlayer().name}'s mark to column ${column} row ${row}.`);
-    } else {
-      console.log("Choose again!");
-    }
 
-    if (winCondition() !== true) {
-      return switchPlayer(), printNewRound;
-    } else {
-      return;
+  function resetBoard() {
+    const boardArr = board.getBoard();
+    for (let row of boardArr) {
+      for (let cell of row) {
+        cell.addMark(0);
+      }
+    }
+    gameOver = false;
+    Display();
+  }
+
+  function Display() {
+    const viewBoard = board.getBoard().flat().map(cell => cell.getValue());
+    for (let i = 0; i < 9; i++) {
+      const tile = document.querySelector(`#tile-${i+1}`);
+      if (tile) {
+        tile.textContent = viewBoard[i] === 0 ? "" : viewBoard[i];
+      }
     }
   };
-  return { playRound, printNewRound };
-}
 
+  const playRound = (column, row) => {
+    if (gameOver === false) {
+     
+      Display();
+      if (board.getBoard()[row][column].getValue() === 0) {
+        board.placeMark(column, row, getActivePlayer().mark);
+        console.log(`Placing ${getActivePlayer().name}'s mark to column ${column} row ${row}.`);
+        Display();
+      } else {
+        
+        console.log(`Choose again! cannot place ${getActivePlayer().name}'s mark at ${column} row ${row} `);
+      }
+
+      if (winCondition() === false) {
+        switchPlayer();
+        printNewRound();
+      } else {
+      return;
+      }
+    };
+  };
+  return { playRound, printNewRound, Display, resetBoard };
+};
+
+
+
+const clickMark = (event) => {
+  const tile = event.target;
+  const row = parseInt(tile.dataset.row, 10);
+  const col = parseInt(tile.dataset.col, 10);
+  game.playRound(col, row);
+};
 const game = GameController();
 window.playRound = game.playRound;
 
-const play = game.playRound;
-game.playRound(0,0);
-game.playRound(0,1);
-game.playRound(1,0);
-game.playRound(1,1);
-game.playRound(2,0);
 
-function Display() {
-  const board = Gameboard();
+const tiles = document.querySelectorAll(".tiles");
+tiles.forEach(element => element.addEventListener('click', clickMark));
 
-  this.displayBoard = function() {
-    viewBoard = board.getBoard().map(row => row.map(cell => cell.getValue()));
-    //problem getting updated array items in DOM
-    
-    newBoard = viewBoard.flat();
-
-    const display1 = document.querySelector("#tile-1");
-    const display2 = document.querySelector("#tile-2");
-    const display3 = document.querySelector("#tile-3");
-    const display4 = document.querySelector("#tile-4");
-    const display5 = document.querySelector("#tile-5");
-    const display6 = document.querySelector("#tile-6");
-    const display7 = document.querySelector("#tile-7");
-    const display8 = document.querySelector("#tile-8");
-    const display9 = document.querySelector("#tile-9");
-
-    display1.textContent = newBoard[0];
-    display2.textContent = newBoard[1];
-    display3.textContent = newBoard[2];
-    display4.textContent = newBoard[3];
-    display5.textContent = newBoard[4];
-    display6.textContent = newBoard[5];
-    display7.textContent = newBoard[6];
-    display8.textContent = newBoard[7];
-    display9.textContent = newBoard[8];
-    
-  }
-
-};
-
-const board1 = new Display();
-board1.displayBoard();
+const resetButton = document.querySelector("#reset")
+resetButton.addEventListener('click', () => {
+  game.resetBoard();
+});
